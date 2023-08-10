2472323d7619d151139d6b39b8a0c0883a6a5dea
asheavenist <asheavenist@yahoo.com>
2023-08-10 13:48:19 +0700

    Changing action to customized url

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,10 @@
 <h1>CSRF with Session - Post Method</h1>
 
 <p>
-  Gunakan form di bawah jika mengakses melalui <code>http://localhost/models/php-mvc-csrf-fixed/public/</code><br>
-  Semua berjalan sesuai dengan ekspektasi
+  Form ini diakses via <code>http://phpmvccsrf.test</code><br>
+  Dan semua berjalan tidak sesuai ekspektasi
 </p>
-<form action="http://localhost/models/php-mvc-csrf-fixed/public/" method="post">
+<form action="http://phpmvccsrf.test" method="post">
   <label for="form-token">csrf-token</label>
   <input type="text" name="form-token" id="form-token" value="<?= $data['form-token'] ?>" style="width: 30rem"><br />
   <button type="submit" name="submit" style="margin-top: 1rem">Submit</button>
