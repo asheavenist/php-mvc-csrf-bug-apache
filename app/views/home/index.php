cfc3e4163266bd58375051081b3d36d361c6a411
asheavenist <asheavenist@yahoo.com>
2025-03-06 07:38:59 +0700

    Memperbarui link agar fitur post berjalan

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,10 @@
 <h1>CSRF with Session - Post Method</h1>
 
 <p>
-  Gunakan form di bawah jika mengakses melalui <code>http://localhost/models/php-mvc-csrf-fixed/public/</code><br>
+  Gunakan form di bawah jika mengakses melalui <code>http://localhost/practice/bug-csrf/public/</code><br>
   Semua berjalan sesuai dengan ekspektasi
 </p>
-<form action="http://localhost/models/php-mvc-csrf-fixed/public/" method="post">
+<form action="http://localhost/practice/bug-csrf/public/" method="post">
   <label for="form-token">csrf-token</label>
   <input type="text" name="form-token" id="form-token" value="<?= $data['form-token'] ?>" style="width: 30rem"><br />
   <button type="submit" name="submit" style="margin-top: 1rem">Submit</button>
